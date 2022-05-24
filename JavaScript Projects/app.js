0f8ae8ee38c5a3c950b29db16d1acaa1e45e6cc7
Syed Sumaim Ali <96646046+SyedSumaimaly@users.noreply.github.com>
2022-05-24 23:09:26 +0500

    Add files via upload

---
+++ JavaScript Projects/app.js
@@ -0,0 +1,42 @@
+// Chap 01
+// Question 1
+
+alert("Hello User")
+
+// Question 2
+
+alert("Error! Please enter a valid password")
+
+// Question 3
+
+alert("Welcome to JS Land.... \n Happy Coding")
+
+// Chap 02
+// Question 1
+
+var username = "Sumaim"
+
+// Question 2
+
+var myName = "Sumaim"
+
+// Question 3
+
+var msg = "Hello World"
+
+alert(msg)
+
+// Question 6
+
+var email = "syedsumaimaly08@gmail.com"
+
+alert('my email address is ' + email)
+
+// Question 7
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn a book " +book)
+
+// Question 8
+
+document.write("Yah! I can write HTML content through Javascript ")
