f364a8d819d1c3a76a6db6544949dd3321fdc2df
Syed Sumaim Ali <96646046+SyedSumaimaly@users.noreply.github.com>
2022-06-01 13:23:05 +0500

    Add files via upload

--- JavaScript Projects/app.js
+++ JavaScript Projects/app.js
@@ -39,4 +39,18 @@ alert("I am trying to learn a book " +book)
 
 // Question 8
 
-document.write("Yah! I can write HTML content through Javascript ")
+// document.write("Yah! I can write HTML content through Javascript ")
+
+// Chap 02
+// Question 1
+
+var age = 17
+alert("I am " + age + " years old");
+
+// Question 4
+
+var name = prompt("Enter your name");
+var product = prompt("Enter product name");
+var quantity = prompt("Enter quantity of a product");
+
+alert(name + " ordered " + quantity + " " + product + " on XYZ Clothing Store");
